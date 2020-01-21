c7c3c46ca43ece13b48ba26826d1b2128a64a329
Tonmoy Ghosh <tonmoyanik65@gmail.com>
2020-01-21 11:30:11 +0600

    api controller create

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function todoView()
+    {
+        return view('todoView');
+    }
+
+    public function todoCreate()
+    {
+        
+    }
+
+    public function todoUpdate($id)
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ApiController@todoView');
+Route::get('/todoCreate', 'ApiController@todoCreate');
+Route::get('/todoUpdate/{id}', 'ApiController@todoUpdate');
